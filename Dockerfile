5f0d4b68f78ededf03e1b5f776d2f299e01a5b48
Mihhail Domkov <mihhail.domkov@noones.team>
2023-10-08 20:05:53 +0300

    fix

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:alpine
-WORKDIR /build/application
-COPY package*.json .
-RUN npm install
-COPY . .
-RUN npm i
-RUN npm run build
-CMD ["node", "./build/index.js"]
---
+++ IaaC.tf
@@ -0,0 +1,82 @@
+# Configure the Google Cloud provider
+provider "google" {
+  credentials = file("auth.json")
+  project     = "infrastructure-task"
+  region      = "us-central1"
+}
+
+# Define a VPC network
+resource "google_compute_network" "my_network" {
+  name = "my-network"
+}
+
+# Define a subnetwork within the VPC
+resource "google_compute_subnetwork" "my_subnetwork" {
+  name          = "my-subnetwork"
+  region        = "us-central1"
+  network       = google_compute_network.my_network.name
+  ip_cidr_range = "10.0.0.0/24"  # Adjust the IP range as needed
+}
+
+# Define a GKE cluster
+resource "google_container_cluster" "my_cluster" {
+  name               = "my-cluster"
+  location           = "us-central1"
+  initial_node_count = 1
+  network            = google_compute_network.my_network.name
+  subnetwork         = google_compute_subnetwork.my_subnetwork.name
+
+  }
+
+# Define a Cloud SQL instance (MySQL in this example)
+resource "google_sql_database_instance" "my_sql_instance" {
+  name             = "my-sql-instance"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+# Define a database inside the Cloud SQL instance
+resource "google_sql_database" "my_database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.my_sql_instance.name
+}
+
+# Create a GCP firewall rule to allow VPN access
+resource "google_compute_firewall" "vpn_firewall_rule" {
+  name    = "allow-vpn-access"
+  network = google_compute_network.my_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["500", "4500"]
+  }
+
+  source_ranges = ["10.26.32.12/32", "19.104.105.29/32"]
+}
+
+# Create a GCP firewall rule to allow HTTP traffic from anywhere
+resource "google_compute_firewall" "http_firewall_rule" {
+  name    = "allow-http-traffic"
+  network = google_compute_network.my_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Output the Kubernetes cluster and Cloud SQL instance information
+output "cluster_endpoint" {
+  value       = google_container_cluster.my_cluster.endpoint
+  description = "Kubernetes cluster endpoint"
+}
+
+output "sql_instance_connection_name" {
+  value       = google_sql_database_instance.my_sql_instance.connection_name
+  description = "Cloud SQL instance connection name"
+} 
